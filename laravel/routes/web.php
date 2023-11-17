4ecce01975a1071909ab7c817b3c019bc8a08415
Dorothy <dorothycheruiyot97@gmail.com>
2023-11-17 08:16:56 +0300

    web routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -15,8 +17,25 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts=[];
+    // only if you are logged in  do you get the list of posts
+    if (auth()->check()){
+        $posts = auth()->user()->UsersCoolPosts()->latest()->get();
+
+    }
+    
+    // $posts = Post::where('user_id',auth()->id()->get());
+    return view('home', ['posts'=>$posts]);    
 });
 
 Route::post('/register',[UserController::class, 'register']);
+Route:: post('/logout',[UserController::class,'logout']);
+Route:: post('/login',[UserController::class,'login']);
+
+
+// posts routes
+Route::post('/create-post',[PostController::class,'createPost']);
+Route::get('/edit-post/{post}',[PostController::class,'showEditScreen']);
+Route::put('/edit-post/{post}',[PostController::class,'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}',[PostController::class,'deletePost']);
 
